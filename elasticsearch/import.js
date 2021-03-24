f811af83c570e861059e65fa91fa8a70a1e58732
bastantoine <bastien.antoine69+github@gmail.com>
2021-03-24 19:13:22 +0100

    Updated script to populate the ES DB

--- elasticsearch/import.js
+++ elasticsearch/import.js
@@ -10,32 +10,87 @@ async function run() {
   const client = new Client({ node: ELASTIC_SEARCH_URI});
 
   // Drop index if exists
+  console.log(`Dropping index ${INDEX_NAME}`);
   await client.indices.delete({
     index: INDEX_NAME,
     ignore_unavailable: true
   });
+  console.log(`└ Dropped index ${INDEX_NAME}`);
 
+  console.log(`Creating index ${INDEX_NAME}`);
   await client.indices.create({
     index: INDEX_NAME,
     body : {
-      // TODO configurer l'index https://www.elastic.co/guide/en/elasticsearch/reference/current/mapping.html
+      mappings: {
+        properties: {
+          point: { type: 'geo_point' },
+          desc: { type: 'text' },
+          zip: { type: 'integer' },
+          category: {
+            type: 'text',
+            // Setup multi-type field to be able to aggregate on the category.
+            // From https://www.elastic.co/guide/en/elasticsearch/reference/current/multi-fields.html
+            fields: {
+              raw: {
+                type: "keyword"
+              }
+            }
+          },
+          title: {
+            type: 'text',
+            // Setup multi-type field to be able to aggregate on the category.
+            // From https://www.elastic.co/guide/en/elasticsearch/reference/current/multi-fields.html
+            fields: {
+              raw: {
+                type: "keyword"
+              }
+            }
+          },
+          timeStamp: {
+            type: 'date',
+            format: "yyyy-MM-dd HH:mm:ss"
+          },
+          twp: {
+            type: 'text',
+            // Setup multi-type field to be able to aggregate on the category.
+            // From https://www.elastic.co/guide/en/elasticsearch/reference/current/multi-fields.html
+            fields: {
+              raw: {
+                type: "keyword"
+              }
+            }
+          },
+          addr: { type: 'text' }
+        }
+      }
     }
   });
+  console.log(`└ Created index ${INDEX_NAME}`);
 
+  let all_calls = [];
   fs.createReadStream('../911.csv')
     .pipe(csv())
     .on('data', data => {
-      const call = { 
-      };
-      // TODO créer l'objet call à partir de la ligne
+      all_calls.push({
+        point: { lat: data.lat, lon: data.lng },
+        desc: data.desc,
+        zip: data.zip,
+        category: data.title.split(':')[0],
+        title: data.title,
+        timeStamp: data.timeStamp,
+        twp: data.twp,
+        addr: data.addr
+      });
     })
     .on('end', async () => {
-      // TODO insérer les données dans ES en utilisant l'API de bulk https://www.elastic.co/guide/en/elasticsearch/reference/7.x/docs-bulk.html
+      console.log(`${all_calls.length} calls to insert`);
+      const body = all_calls.flatMap((call) => [{ index: { _index: INDEX_NAME, _type: '_doc' } }, call]);
+      client.bulk({ body }, (err, resp) => {
+        if (err) console.trace(err.message);
+        else console.log(`└ ${resp.body.items.length} calls inserted`);
+        client.close();
+      })
     });
-  
-
 }
 
 run().catch(console.log);
-
-
